0fae2bc94d8408f49cf8b31774fb1908b7444ae9
Gabriel Tonete <gabtopi77@gmail.com>
2022-02-18 13:50:50 -0300

    cors

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,1 +1,9 @@
-app.use(require('cors')());
+const cors = require('cors');
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    app.use(cors());
+    next();
+});
